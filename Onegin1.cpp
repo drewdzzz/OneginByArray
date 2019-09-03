ed505703e1eb487dd41665eba4f57beaddb60908
drewdzzz <54822306+drewdzzz@users.noreply.github.com>
2019-09-03 18:28:13 +0300

    Update Onegin1.cpp

--- Onegin1.cpp
+++ Onegin1.cpp
@@ -7,59 +7,7 @@
 #define STRING_LIMIT 50
 //#define DEBUG
 
-//void quickSort ( char poem[SIZE][STRING_LIMIT], int left, int right)
-/*{
-    assert ( poem != NULL );
-    assert ( isfinite (left) );
-    assert ( isfinite (right));
-    assert ( left >= 0 && left < STRING_LIMIT);
-    assert ( right >= 0 && right < STRING_LIMIT);
-
-    int strcmp (char*, char*);
-    void mySwap ( char [SIZE][STRING_LIMIT], int, int);
-
-    char* mid = poem[ (left + right) / 2];
-    int l = left;
-    int r = right;
-    while ( right > left )
-    {
-        while ( strcmp (poem[left], mid) < 0 )   left++;
-        while ( strcmp (poem[right], mid) > 0 ) right--;
-        if (left < right) mySwap ( poem, left, right);
-        left++;
-        right--;
-    }
-    if ( l < right ) quickSort( poem, l, right);
-    if ( r > left  ) quickSort( poem, left, r);
-}
-*/
-//{----------------------------------------------------------------------------
-//!
-//! @brief Функция позволяет поместить поэму в массив
-//!
-//! @param fp поток, из которого осуществляется ввод
-//!
-//! @param poem двумерный массив типа char, в который будет осуществляться ввод
-//!
-//! @param *sizeofpoem параметр, в который передаётся размер кол-во строк (ограничивается константой STRING_LIMIT)
-//!
-//! @par как юзать:
-//! @code
-//! readInput ( fp, poem, &sizeofpoem);
-//! @endcode
-//}----------------------------------------------------------------------------
-
 void readInput ( FILE* fp, char poem[SIZE][STRING_LIMIT], int* sizeofpoem );
-
-//{----------------------------------------------------------------------------
-//!
-//! @brief An usual BubbleSort:)
-//!
-//! @param poem double-dimensional array that will be sorted
-//!
-//! @param arraySize size of sorted array
-//}----------------------------------------------------------------------------
-
 void bubbleSort ( char poem [SIZE][STRING_LIMIT], int arraySize );
 void mySwap (char mySwappingArray[SIZE][STRING_LIMIT], int a, int b);
 int main ()
@@ -81,7 +29,6 @@ int main ()
     #endif
 
     bubbleSort ( poem, sizeofpoem);
-    //quickSort ( poem, 0, sizeofpoem-1);
     #ifdef DEBUG
     printf ("Poem Sorted!");
     #endif
@@ -131,19 +78,6 @@ void bubbleSort( char poem [SIZE][STRING_LIMIT], int arraySize)
             }
 }
 
-
-
-
-//{----------------------------------------------------------------------------
-//! @brief Компоратор строк (понижает регистры)
-//!
-//! @return Возвращает разницу кодов между первыми попавшимися разными символами строк (0 если равны, >0 если a>b, <0 если a<b)
-//!
-//!
-//!
-//!
-//}----------------------------------------------------------------------------
-
 int strcmp ( char* string1, char* string2 )
 {
     assert ( string1 != NULL );
@@ -162,17 +96,6 @@ int strcmp ( char* string1, char* string2 )
 
     return string1[i] - string2[i];
 }
-
-
-//{----------------------------------------------------------------------------
-//!
-//! @brief Функция меняется местами два элемента в массиве char
-//!
-//! @param array массив, в котором меняются элементы
-//! @param a индекс первого элемента
-//! @param b индекс второго элемента
-//!
-//}----------------------------------------------------------------------------
 
 void mySwap (char mySwappingArray[SIZE][STRING_LIMIT], int a, int b)
 {
