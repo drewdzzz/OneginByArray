96cff22e3b35d43e295295bcc0366f6b8cc5766a
drewdzzz <54822306+drewdzzz@users.noreply.github.com>
2019-09-04 00:54:31 +0300

    Update Onegin1.cpp

--- Onegin1.cpp
+++ Onegin1.cpp
@@ -3,80 +3,118 @@
 #include <math.h>
 #define INPUTFILE_NAME  "onegin.txt"
 #define OUTPUTFILE_NAME "oneginOut.txt"
-#define SIZE         50
-#define STRING_LIMIT 50
+#define MAX_STRING_SIZE 50
+#define MAX_STRING_NUM  50
 //#define DEBUG
 
-void readInput ( FILE* fp, char poem[SIZE][STRING_LIMIT], int* sizeofpoem );
-void bubbleSort ( char poem [SIZE][STRING_LIMIT], int arraySize );
-void mySwap (char mySwappingArray[SIZE][STRING_LIMIT], int a, int b);
+//{----------------------------------------------------------------------------
+//!
+//! @brief Функция позволяет поместить поэму в массив
+//!
+//! @param fp поток, из которого осуществляется ввод
+//!
+//! @param poem двумерный массив типа char, в который будет осуществляться ввод
+//!
+//! @param *lines_in_poem параметр, в который передаётся размер кол-во строк (ограничивается константой MAX_STRING_NUM)
+//!
+//! @par как юзать:
+//! @code
+//! readInput ( fp, poem, &lines_in_poem);
+//! @endcode
+//}----------------------------------------------------------------------------
+
+void readInput ( FILE* fp, char poem[MAX_STRING_NUM][MAX_STRING_SIZE], int* lines_in_poem );
+
+//{----------------------------------------------------------------------------
+//!
+//! @brief An usual BubbleSort:)
+//!
+//! @param poem double-dimensional array that will be sorted
+//!
+//! @param arraySize size of sorted array
+//}----------------------------------------------------------------------------
+
+void bubbleSort ( char poem [MAX_STRING_NUM][MAX_STRING_SIZE], int arraySize );
+void mySwap (char mySwappingArray[MAX_STRING_NUM][MAX_STRING_SIZE], int a, int b);
+int strcmp (char*, char*);
 int main ()
 {
     FILE* fp;
     if ( !(fp = fopen ( INPUTFILE_NAME, "r")) )
     {
-        printf ("Input file is not open");
+        fprintf (stderr,"Input file is not open");
         return 1;
     }
-    int sizeofpoem = STRING_LIMIT;
-    char poem[SIZE][STRING_LIMIT];
-    readInput ( fp, poem, &sizeofpoem );
+    int lines_in_poem = MAX_STRING_NUM;
+    char poem[MAX_STRING_NUM][MAX_STRING_SIZE];
+    readInput ( fp, poem, &lines_in_poem );
 
     #ifdef DEBUG
     printf ("Reading complete!\n");
-            for ( int i = 0; i < sizeofpoem; i++)
+            for ( int i = 0; i < lines_in_poem; i++)
             puts(poem[i]);
     #endif
 
-    bubbleSort ( poem, sizeofpoem);
+    bubbleSort ( poem, lines_in_poem);
     #ifdef DEBUG
     printf ("Poem Sorted!");
     #endif
     fclose(fp);
     if ( !(fp = fopen ( OUTPUTFILE_NAME, "w")) )
     {
-        printf ("Output file is not open");
+        fprintf (stderr, "Output file is not open");
         return 1;
     }
-    for (int i = 0; i < sizeofpoem; i++)
+    for (int i = 0; i < lines_in_poem; i++)
             fputs ( poem[i], fp);
 
     return 0;
 }
 
-void readInput ( FILE* fp, char inputArray[SIZE][STRING_LIMIT], int* sizeofpoem)
+void readInput ( FILE* fp, char inputArray[MAX_STRING_NUM][MAX_STRING_SIZE], int* lines_in_poem)
 {
-    assert (isfinite(*sizeofpoem));
+    assert (isfinite(*lines_in_poem));
     assert (!feof(fp));
 
-
-    for (int i = 0; i < *sizeofpoem; i++)
+    int poemlines = *lines_in_poem;
+    for (int i = 0; i < poemlines; i++)
         if (!feof (fp))
-            fgets ( inputArray[i], SIZE, fp);
+            fgets ( inputArray[i], MAX_STRING_SIZE, fp);
         else
         {
-            *sizeofpoem = i;
+            *lines_in_poem = i;
             return;
         }
 }
 
 
-void bubbleSort( char poem [SIZE][STRING_LIMIT], int arraySize)
+void bubbleSort( char poem [MAX_STRING_NUM][MAX_STRING_SIZE], int arraySize)
 {
     assert ( poem != NULL );
     assert ( isfinite (arraySize) );
-    int strcmp (char*, char*);
-    void mySwap ( char [SIZE][STRING_LIMIT], int, int);
     for (int i = 0; i < arraySize; i++)
         for (int j = i; j < arraySize; j++)
             if ( strcmp ( poem[i], poem[j]) > 0 )
             {
                 mySwap (poem, i, j);
                 #ifdef DEBUG
-                    printf("SWAPPED!\n");
+                    printf("mySwapPED!\n");
                 #endif
             }
 }
+
+
+
+
+//{----------------------------------------------------------------------------
+//! @brief Компоратор строк (понижает регистры)
+//!
+//! @return Возвращает разницу кодов между первыми попавшимися разными символами строк (0 если равны, >0 если a>b, <0 если a<b)
+//!
+//!
+//!
+//!
+//}----------------------------------------------------------------------------
 
 int strcmp ( char* string1, char* string2 )
 {
@@ -97,7 +135,18 @@ int strcmp ( char* string1, char* string2 )
     return string1[i] - string2[i];
 }
 
-void mySwap (char mySwappingArray[SIZE][STRING_LIMIT], int a, int b)
+
+//{----------------------------------------------------------------------------
+//!
+//! @brief Функция меняется местами два элемента в массиве char
+//!
+//! @param array массив, в котором меняются элементы
+//! @param a индекс первого элемента
+//! @param b индекс второго элемента
+//!
+//}----------------------------------------------------------------------------
+
+void mySwap (char mySwappingArray[MAX_STRING_NUM][MAX_STRING_SIZE], int a, int b)
 {
     assert (isfinite(a));
     assert (isfinite(b));
@@ -105,11 +154,11 @@ void mySwap (char mySwappingArray[SIZE][STRING_LIMIT], int a, int b)
     #ifdef DEBUG
         printf("IS mySwapPING...\n");
     #endif
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < MAX_STRING_SIZE; i++)
     {
-        char temp = mySwappingArray[a][i];
-        mySwappingArray[a][i] = mySwappingArray[b][i];
-        mySwappingArray[b][i] = temp;
+        mySwappingArray[a][i] += mySwappingArray[b][i];
+        mySwappingArray[b][i] = mySwappingArray[a][i] - mySwappingArray[b][i];
+        mySwappingArray[a][i] -= mySwappingArray[b][i];
     }
 }
 
